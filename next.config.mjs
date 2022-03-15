d772baa37b3fd90ddc902cb43d1dbaf79db69d07
defikri <dobeon.com@gmail.com>
2022-03-15 09:42:13 +0700

    chore: bootstrap Next.js 14 + TypeScript project

---
+++ next.config.mjs
@@ -0,0 +1,49 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        '*.localhost:3000',
+        'localhost',
+        '*.localhost',
+      ],
+    },
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'http', hostname: 'localhost' },
+      { protocol: 'https', hostname: '*.r2.cloudflarestorage.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'ui-avatars.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'plus.unsplash.com' },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(self), interest-cohort=()',
+          },
+        ],
+      },
+    ]
+  },
+  // NOTE: Multi-tenant subdomain rewrites are handled in middleware.ts
+}
+
+export default nextConfig
